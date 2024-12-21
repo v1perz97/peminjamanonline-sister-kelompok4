a352c01fa436d4ef395d0389a61c91db76ee9ec9
Tresnanda A.C.B <zionexeon@gmail.com>
2024-12-21 12:03:26 +0700

    Aplikasi Peminjaman Online

---
+++ src/main/java/com/mycompany/peminjamanonline_sister_kelompok4/Peminjamanonline_sister_kelompok4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.peminjamanonline_sister_kelompok4;
+
+/**
+ *
+ * @author Zion Exeon Ch
+ */
+public class Peminjamanonline_sister_kelompok4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
